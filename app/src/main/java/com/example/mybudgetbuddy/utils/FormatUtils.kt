978f67d1ff39ee1a4d5645b61f0cdfd08a1c720b
Kattis91 <katya.svedmark@gmail.com>
2025-03-21 12:59:25 +0100

    Update formatAmount fun to handle null

--- app/src/main/java/com/example/mybudgetbuddy/utils/FormatUtils.kt
+++ app/src/main/java/com/example/mybudgetbuddy/utils/FormatUtils.kt
@@ -3,10 +3,10 @@ package com.example.mybudgetbuddy.utils
 import java.text.NumberFormat
 import java.util.Locale
 
-fun formatAmount(amount: Double): String {
+fun formatAmount(amount: Double?): String {
     val formatter = NumberFormat.getNumberInstance(Locale.FRANCE).apply {
         minimumFractionDigits = 2
         maximumFractionDigits = 2
     }
-    return formatter.format(amount)
+    return formatter.format(amount ?: 0.0)
 }
