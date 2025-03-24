a41636f63277a1aa8cf14e0c6fe4ea85a09d5325
Kattis91 <katya.svedmark@gmail.com>
2025-03-24 13:37:19 +0100

    Update the style of the tab bar

--- app/src/main/java/com/example/mybudgetbuddy/components/TabBar.kt
+++ app/src/main/java/com/example/mybudgetbuddy/components/TabBar.kt
@@ -1,22 +1,28 @@
 package com.example.mybudgetbuddy.components
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.BarChart
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material.icons.filled.RemoveCircle
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.filled.StackedBarChart
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.mybudgetbuddy.R
 import com.example.mybudgetbuddy.TabItem
 
 @Composable
@@ -31,7 +37,27 @@ fun TabBar(navController: NavController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
 
-    NavigationBar {
+    val isDarkMode = isSystemInDarkTheme()
+
+    val gradientColors = if (isDarkMode) {
+        listOf(Color(40, 40, 45), Color(28, 28, 32))
+    } else {
+        listOf(Color(252, 242, 230), Color(245, 235, 220))
+    }
+
+    NavigationBar(
+        modifier = Modifier.drawBehind {
+            drawRect(
+                brush = Brush.linearGradient(
+                    colors = gradientColors,
+                    start = Offset(0f, 0f),
+                    end = Offset(1000f, 1000f)
+                )
+            )
+        },
+        containerColor = Color.Transparent, // Set containerColor to transparent
+        tonalElevation = 0.dp // Remove tonal elevation
+    ) {
         items.forEach { item ->
             NavigationBarItem(
                 icon = { Icon(item.icon, contentDescription = item.title) },
@@ -52,7 +78,14 @@ fun TabBar(navController: NavController) {
                             restoreState = true
                         }
                     }
-                }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = colorResource(id = R.color.expense_color),
+                    unselectedIconColor = colorResource(id = R.color.background_tint_dark),
+                    selectedTextColor = colorResource(id = R.color.expense_color),
+                    unselectedTextColor = colorResource(id = R.color.background_tint_dark),
+                    indicatorColor = Color.Transparent
+                )
             )
         }
     }
