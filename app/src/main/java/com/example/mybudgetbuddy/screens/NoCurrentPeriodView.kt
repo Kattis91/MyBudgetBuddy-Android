863eb0700f37d280caf659f24dc924a03108e578
Kattis91 <katya.svedmark@gmail.com>
2025-03-25 10:51:17 +0100

    Create a view that will be shown when there is no current period

---
+++ app/src/main/java/com/example/mybudgetbuddy/screens/NoCurrentPeriodView.kt
@@ -0,0 +1,102 @@
+package com.example.mybudgetbuddy.screens
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mybudgetbuddy.NewBudgetPeriodView
+import com.example.mybudgetbuddy.R
+import com.example.mybudgetbuddy.components.CustomButton
+
+@Composable
+fun NoCurrentPeriodScreen(onPeriodCreated: () -> Unit) {
+
+    var showNewPeriodDialog by remember { mutableStateOf(false) }
+
+    val isDarkMode = isSystemInDarkTheme()
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Icon(
+            painter = painterResource(id = R.drawable.savings),
+            contentDescription = "App Logo",
+            modifier = Modifier
+                .height(200.dp)
+                .width(200.dp)
+                .padding(bottom = 16.dp),
+            tint = Color.Unspecified
+        )
+
+        Text(
+            "Welcome to MyBudgetBuddy!",
+            fontSize = 25.sp,
+            modifier = Modifier.padding(top = 16.dp),
+            color = if (isDarkMode) Color.White else colorResource(id = R.color.text_color)
+        )
+
+        Spacer(modifier = Modifier.height(26.dp))
+
+        Text(
+            "To start tracking your budget, you'll need to create your first budget period.",
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(horizontal = 10.dp),
+            color = if (isDarkMode) Color.White else colorResource(id = R.color.text_color),
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(26.dp))
+
+        CustomButton(
+            onClick = {
+                showNewPeriodDialog = true
+            },
+            buttonText = "Create a new period",
+            isIncome = false,
+            isExpense = true,
+            isThirdButton = false,
+            width = 215
+        )
+
+        // Show New Budget Period Dialog when button is clicked
+        if (showNewPeriodDialog) {
+            NewBudgetPeriodView(
+                isPresented = true,
+                onDismiss = { showNewPeriodDialog = false },
+                onSuccess = {
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoCurrentPeriodScreenPreview() {
+    NoCurrentPeriodScreen(onPeriodCreated = {})
+}
+
