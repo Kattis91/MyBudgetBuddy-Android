b87c285cb3372d37f8c591c193f178c35ebb75b0
Kattis91 <katya.svedmark@gmail.com>
2025-03-25 17:31:54 +0100

    Create reusable FeatureCard to use in the InfoScreen

---
+++ app/src/main/java/com/example/mybudgetbuddy/components/FeatureCard.kt
@@ -0,0 +1,52 @@
+package com.example.mybudgetbuddy.components
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.mybudgetbuddy.R
+
+@Composable
+fun FeatureCard(
+    icon: ImageVector,
+    text: String
+) {
+    val isDarkMode = isSystemInDarkTheme()
+    val textColor = if (isDarkMode) Color.White else colorResource(id = R.color.text_color)
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .padding(vertical = 8.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ){
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = ""
+            )
+            Text(
+                text = text,
+                modifier = Modifier.padding(16.dp),
+                color = textColor
+            )
+        }
+    }
+}
