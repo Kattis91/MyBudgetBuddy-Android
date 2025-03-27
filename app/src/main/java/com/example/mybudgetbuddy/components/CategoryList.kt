696466ce0f98672680cc6be6fb8ebbf27a4d54b6
Kattis91 <katya.svedmark@gmail.com>
2025-03-27 16:20:31 +0100

    Add edit and delete icons

--- app/src/main/java/com/example/mybudgetbuddy/components/CategoryList.kt
+++ app/src/main/java/com/example/mybudgetbuddy/components/CategoryList.kt
@@ -1,21 +1,34 @@
 package com.example.mybudgetbuddy.components
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import com.example.mybudgetbuddy.R
 
 @Composable
 fun CategoryList(
     categories: List<String>,
 ) {
+    val isDarkMode = isSystemInDarkTheme()
+
     LazyColumn {
         items(categories) { category ->
             Card(
@@ -25,9 +38,32 @@ fun CategoryList(
                     .padding(vertical = 8.dp),
                 elevation = CardDefaults.cardElevation(4.dp)
             ) {
-                Column(modifier = Modifier.padding(16.dp)
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                        .padding(vertical = 6.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(text = category)
+
+                    Row {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Default.Edit,
+                                contentDescription = "Edit",
+                                tint = if(isDarkMode) Color.White else Color.Blue
+                            )
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = "Delete",
+                                tint = colorResource(id = R.color.error_message_color)
+                            )
+                        }
+                    }
                 }
             }
         }
