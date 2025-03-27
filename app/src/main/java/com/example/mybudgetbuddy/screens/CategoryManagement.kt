71c80ced9ef461ccf8b3c06a2770b4c0656824e6
Kattis91 <katya.svedmark@gmail.com>
2025-03-27 14:16:22 +0100

    Display categories in respective selectedTab (no style yet)

--- app/src/main/java/com/example/mybudgetbuddy/screens/CategoryManagement.kt
+++ app/src/main/java/com/example/mybudgetbuddy/screens/CategoryManagement.kt
@@ -5,8 +5,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -16,12 +20,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mybudgetbuddy.budget.BudgetManager
 import com.example.mybudgetbuddy.components.SegmentedButtonRow
 
 @Composable
-fun CategoryManagement() {
+fun CategoryManagement(
+    viewModel: BudgetManager = viewModel()
+) {
 
     var selectedTabIndex by remember { mutableStateOf(0) }
+    val incomeCategories by viewModel.incomeCategories.collectAsState()
+    val fixedExpenseCategories by viewModel.fixedExpenseCategories.collectAsState()
+    val variableExpenseCategories by viewModel.variableExpenseCategories.collectAsState()
+
+    LaunchedEffect(Unit) {
+        viewModel.loadIncomeCategories()
+        viewModel.loadFixedExpenseCategories()
+        viewModel.loadVariableExpenseCategories()
+    }
 
     Column(modifier = Modifier
         .fillMaxSize()
@@ -29,7 +46,8 @@ fun CategoryManagement() {
         horizontalAlignment = Alignment.CenterHorizontally
     )  {
 
-        Text("Manage Categories",
+        Text(
+            "Manage Categories",
             fontSize = 20.sp,
         )
 
@@ -47,13 +65,25 @@ fun CategoryManagement() {
 
         when (selectedTabIndex) {
             0 -> {
-                Text("Income Categories")
+                LazyColumn {
+                    items(incomeCategories) { category ->
+                        Text(category)
+                    }
+                }
             }
             1 -> {
-                Text("Fixed Expense Categories")
+                LazyColumn {
+                    items(fixedExpenseCategories) { category ->
+                        Text(category)
+                    }
+                }
             }
             2 -> {
-                Text("Variable Expense Categories")
+                LazyColumn {
+                    items(variableExpenseCategories) { category ->
+                        Text(category)
+                    }
+                }
             }
         }
     }
