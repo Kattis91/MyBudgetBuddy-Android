935f9c8d792e611723eb17d0df22fa01e66cd6db
Kattis91 <katya.svedmark@gmail.com>
2025-04-01 20:47:43 +0200

    Add an arrow icon too show that periods are clickable

--- app/src/main/java/com/example/mybudgetbuddy/components/PeriodRowView.kt
+++ app/src/main/java/com/example/mybudgetbuddy/components/PeriodRowView.kt
@@ -1,15 +1,21 @@
 package com.example.mybudgetbuddy.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -37,16 +43,30 @@ fun PeriodRowView(
             },
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                formattedDateRange(period.startDate, period.endDate),
-                fontWeight = FontWeight.Bold,
-                color = textColor
-            )
-            Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    formattedDateRange(period.startDate, period.endDate),
+                    fontWeight = FontWeight.Bold,
+                    color = textColor
+                )
+                Spacer(modifier = Modifier.height(8.dp))
 
-            Text("Outcome: ${formatAmount(period.totalIncome - (period.totalFixedExpenses + period.totalVariableExpenses))}",
-                color = textColor)
+                Text(
+                    "Outcome: ${formatAmount(period.totalIncome - (period.totalFixedExpenses + period.totalVariableExpenses))}",
+                    color = textColor
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Column(modifier = Modifier.padding(end = 5.dp)) {
+                Icon(
+                    Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                    contentDescription = "Right arrow"
+                )
+            }
         }
     }
 }
