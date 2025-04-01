48415d9e600d8fe5a0fba5dc65bd2b06423890ec
Kattis91 <katya.svedmark@gmail.com>
2025-04-01 20:40:34 +0200

    Refactor the code by creating separate PeriodRowView

---
+++ app/src/main/java/com/example/mybudgetbuddy/components/PeriodRowView.kt
@@ -0,0 +1,64 @@
+package com.example.mybudgetbuddy.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.mybudgetbuddy.models.BudgetPeriod
+import com.example.mybudgetbuddy.utils.formatAmount
+import com.example.mybudgetbuddy.utils.formattedDateRange
+import java.util.Date
+
+@Composable
+fun PeriodRowView(
+    period: BudgetPeriod,
+    navController: NavController,
+    textColor: Color
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .clickable {
+                navController.navigate("periodDetail/${period.id}")
+            },
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                formattedDateRange(period.startDate, period.endDate),
+                fontWeight = FontWeight.Bold,
+                color = textColor
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text("Outcome: ${formatAmount(period.totalIncome - (period.totalFixedExpenses + period.totalVariableExpenses))}",
+                color = textColor)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PeriodRowViewPreview() {
+    val previewNavController = rememberNavController()
+    PeriodRowView(
+        navController = previewNavController,
+        period = BudgetPeriod(startDate = Date(), endDate = Date()),
+        textColor = Color.Blue)
+}
+
+
--- app/src/main/java/com/example/mybudgetbuddy/screens/OverviewTabView.kt
+++ app/src/main/java/com/example/mybudgetbuddy/screens/OverviewTabView.kt
@@ -1,16 +1,12 @@
 package com.example.mybudgetbuddy.screens
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -18,7 +14,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,10 +22,9 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.mybudgetbuddy.R
 import com.example.mybudgetbuddy.budget.BudgetManager
+import com.example.mybudgetbuddy.components.PeriodRowView
 import com.example.mybudgetbuddy.components.SummaryBox
 import com.example.mybudgetbuddy.models.BudgetPeriod
-import com.example.mybudgetbuddy.utils.formatAmount
-import com.example.mybudgetbuddy.utils.formattedDateRange
 import java.util.Date
 
 @Composable
@@ -63,27 +57,7 @@ fun OverviewTabView(
 
         LazyColumn {
             items(nonEmptyPeriods) { period ->
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp)
-                        .clickable {
-                            navController.navigate("periodDetail/${period.id}")
-                        },
-                    elevation = CardDefaults.cardElevation(4.dp)
-                ) {
-                    Column(modifier = Modifier.padding(16.dp)) {
-                        Text(
-                            formattedDateRange(period.startDate, period.endDate),
-                            fontWeight = FontWeight.Bold,
-                            color = textColor
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-
-                        Text("Outcome: ${formatAmount(period.totalIncome - (period.totalFixedExpenses + period.totalVariableExpenses))}",
-                            color = textColor)
-                    }
-                }
+                PeriodRowView(period = period, navController = navController, textColor = textColor)
             }
         }
     }
