0c5dc0eb3dc23a5f0f79b2811a3bf5f057d6b7f9
Kattis91 <katya.svedmark@gmail.com>
2025-04-02 17:49:02 +0200

    Request notification permission

--- app/src/main/java/com/example/mybudgetbuddy/MainActivity.kt
+++ app/src/main/java/com/example/mybudgetbuddy/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.mybudgetbuddy
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 
 import com.example.mybudgetbuddy.ui.theme.MyBudgetBuddyTheme
 
@@ -19,6 +24,38 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     MyBudgetBuddyNav()
                 }
+            }
+        }
+        askNotificationPermission()
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Log.i("MyBudgetBuddy", "PERMISSION GRANTED")
+        } else {
+            Log.i("MyBudgetBuddy", "PERMISSION DENIED")
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.i("MyBudgetBuddy", "Permission ALREADY Granted")
+            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+                Log.i("MyBudgetBuddy", "Permission Show UI")
+            } else {
+                // Directly ask for the permission
+                Log.i("MyBudgetBuddy", "Permission ASK USER")
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
             }
         }
     }
