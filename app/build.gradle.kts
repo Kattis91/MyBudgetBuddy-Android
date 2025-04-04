d8bc294ad23a8d08002be2429e3f84c98fc9f506
Kattis91 <katya.svedmark@gmail.com>
2025-04-04 12:54:16 +0200

    Change versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.kat.mybudgetbuddy"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 1.1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
