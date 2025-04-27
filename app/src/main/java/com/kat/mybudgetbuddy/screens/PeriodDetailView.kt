0bb27d02b526bf4ebb960ea0e3f028a957635137
Kattis91 <katya.svedmark@gmail.com>
2025-04-27 15:12:19 +0200

    Display a message if there are no incomes, fixed or variable expenses in the historical period

--- app/src/main/java/com/kat/mybudgetbuddy/screens/PeriodDetailView.kt
+++ app/src/main/java/com/kat/mybudgetbuddy/screens/PeriodDetailView.kt
@@ -1,5 +1,6 @@
 package com.kat.mybudgetbuddy.screens
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,12 +17,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.kat.mybudgetbuddy.R
 import com.kat.mybudgetbuddy.budget.BudgetManager
 import com.kat.mybudgetbuddy.components.AnimatedSegmentedButtonRow
 import com.kat.mybudgetbuddy.components.CustomListView
@@ -35,6 +40,7 @@ fun PeriodDetailView(
 ) {
     var selectedTabIndex by remember { mutableStateOf(0) }
     val historicalPeriods by viewModel.historicalPeriods.collectAsState()
+    val isDarkMode = isSystemInDarkTheme()
 
     // Force load historical periods when this screen is shown
     LaunchedEffect(periodId) {
@@ -82,44 +88,74 @@ fun PeriodDetailView(
         when (selectedTabIndex) {
             0 -> {
                 if (period != null) {
-                    CustomListView(
-                        items = period.incomes,
-                        deleteAction = {},
-                        itemContent = { income ->
-                            Triple(income.category, income.amount, null)
-                        },
-                        showNegativeAmount = false,
-                        alignAmountInMiddle = false,
-                        isInvoice = false
-                    )
+                    if (period.incomes.isNotEmpty()) {
+                        CustomListView(
+                            items = period.incomes,
+                            deleteAction = {},
+                            itemContent = { income ->
+                                Triple(income.category, income.amount, null)
+                            },
+                            showNegativeAmount = false,
+                            alignAmountInMiddle = false,
+                            isInvoice = false
+                        )
+                    } else {
+                        Text(
+                            "You didn't add any incomes for this period.",
+                            fontSize = 23.sp,
+                            color = if (isDarkMode) Color.White else colorResource(R.color.text_color),
+                            modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+                            textAlign = TextAlign.Center
+                        )
+                    }
                 }
             }
             1 -> {
                 if (period != null) {
-                    CustomListView(
-                        items = period.fixedExpenses,
-                        deleteAction = {},
-                        itemContent = { expense ->
-                            Triple(expense.category, expense.amount, null)
-                        },
-                        showNegativeAmount = true,
-                        alignAmountInMiddle = false,
-                        isInvoice = false
-                    )
+                    if (period.fixedExpenses.isNotEmpty()) {
+                        CustomListView(
+                            items = period.fixedExpenses,
+                            deleteAction = {},
+                            itemContent = { expense ->
+                                Triple(expense.category, expense.amount, null)
+                            },
+                            showNegativeAmount = true,
+                            alignAmountInMiddle = false,
+                            isInvoice = false
+                        )
+                    } else {
+                        Text(
+                            "You didn't add any fixed expenses for this period.",
+                            fontSize = 23.sp,
+                            color = if (isDarkMode) Color.White else colorResource(R.color.text_color),
+                            modifier = Modifier.padding(horizontal = 18.dp, vertical = 10.dp),
+                            textAlign = TextAlign.Center
+                        )
+                    }
                 }
             }
             2 -> {
                 if (period != null) {
-                    CustomListView(
-                        items = period.variableExpenses,
-                        deleteAction = {},
-                        itemContent = { expense ->
-                            Triple(expense.category, expense.amount, null)
-                        },
-                        showNegativeAmount = true,
-                        alignAmountInMiddle = false,
-                        isInvoice = false
-                    )
+                    if (period.variableExpenses.isNotEmpty()) {
+                        CustomListView(
+                            items = period.variableExpenses,
+                            deleteAction = {},
+                            itemContent = { expense ->
+                                Triple(expense.category, expense.amount, null)
+                            },
+                            showNegativeAmount = true,
+                            alignAmountInMiddle = false,
+                            isInvoice = false
+                        )
+                    } else {
+                        Text(
+                            "You didn't add any variable expenses for this period.",
+                            fontSize = 23.sp,
+                            color = if (isDarkMode) Color.White else colorResource(R.color.text_color),
+                            modifier = Modifier.padding(horizontal = 18.dp, vertical = 10.dp),
+                            textAlign = TextAlign.Center
+                        )
+                    }
                 }
             }
         }
