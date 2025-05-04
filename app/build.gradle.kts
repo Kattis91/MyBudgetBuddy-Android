ca6319466f43af8d1b28e06bdd86ff7868887b99
Kattis91 <katya.svedmark@gmail.com>
2025-05-04 18:00:23 +0200

    Upgrade versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.kat.mybudgetbuddy"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
